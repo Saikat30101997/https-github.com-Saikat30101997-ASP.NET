ceb6f90893c2efcf5a1c7b0dee796e437c8ff2dd
Saikat Das Tushar <saikat30101997@gmail.com>
2021-03-13 14:53:23 +0600

    Add project files.

---
+++ forconsoletoweb/forconsoletoweb/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+
+namespace forconsoletoweb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public IHostBuilder CreateDefaultBuilder(string[] args)
+        {
+            Host.CreateDefaultBuilder(args);
+        }
+    }
+}
